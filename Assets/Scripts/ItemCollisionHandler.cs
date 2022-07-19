ab124a707d6716402bbc3db709638b09e1f34160
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2022-07-18 21:33:26 -0700

    made balance bar change color to represent load

--- Assets/Scripts/ItemCollisionHandler.cs
+++ Assets/Scripts/ItemCollisionHandler.cs
@@ -54,17 +54,44 @@ public class ItemCollisionHandler : MonoBehaviour
 			case int expression when balanceDirection > 0:
 				leftBalance.value = balanceDirection;
 				rightBalance.value = 0;
-				//Debug.Log("left Bucket" + balanceDirection);
 				break;
 			case int expression when balanceDirection < 0:
 				rightBalance.value = balanceDirection * -1;
 				leftBalance.value = 0;
-				//Debug.Log("right Bucket" + balanceDirection);
 				break;
 			default:
 				Debug.Log("buckets are balanced" + balanceDirection);
 				leftBalance.value = 0;
 				rightBalance.value = 0;
+				break;
+		}
+
+		if (balanceDirection < 0)
+		{
+			balanceDirection = balanceDirection * -1;
+		}
+
+		switch ((float)balanceDirection / (float)balanceCapacity)
+		{
+			case < 0.25f:
+				leftBalance.fillRect.GetComponent<Image>().color = Color.green;
+				rightBalance.fillRect.GetComponent<Image>().color = Color.green;
+				break;
+			case < 0.5f:
+				leftBalance.fillRect.GetComponent<Image>().color = new Color(0.9803922f, 0.7176471f, 0.2f);
+				rightBalance.fillRect.GetComponent<Image>().color = new Color(0.9803922f, 0.7176471f, 0.2f);
+				break;
+			case < 0.75f:
+				leftBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.5568628f, 0.08235294f);
+				rightBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.5568628f, 0.08235294f);
+				break;
+			case < 0.95f:
+				leftBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.3058824f, 0.06666667f);
+				rightBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.3058824f, 0.06666667f);
+				break;
+			case > 0.95f:
+				leftBalance.fillRect.GetComponent<Image>().color = Color.red;
+				rightBalance.fillRect.GetComponent<Image>().color = Color.red;
 				break;
 		}
 
