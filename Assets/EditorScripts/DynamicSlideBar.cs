9eb85773298ef8f810a072f748ca16ba80c91444
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2022-07-19 21:48:31 -0700

    working on making the balance bar better.

---
+++ Assets/EditorScripts/DynamicSlideBar.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEditor;
+
+public class DynamicSlideBar : EditorWindow
+{
+	/// <summary>
+	/// OnGUI is called for rendering and handling GUI events.
+	/// This function can be called multiple times per frame (one call per event).
+	/// </summary>
+	private void OnGUI()
+	{
+
+	}
+}
---
+++ Assets/Scripts/BalanceBar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalanceBar : MonoBehaviour
+{
+	public RectTransform fillRect;
+	public float minValue = 0;
+	public float maxValue = 0;
+	public float value = 0;
+
+
+}
