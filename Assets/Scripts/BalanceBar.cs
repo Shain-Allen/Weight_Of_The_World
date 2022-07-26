986b84984966d5847d830c835cf92121425f3a0a
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2022-07-26 16:32:57 -0700

    setting the Anchors of the Balance Display

--- Assets/Scripts/BalanceBar.cs
+++ Assets/Scripts/BalanceBar.cs
@@ -6,9 +6,31 @@ using UnityEngine.UI;
 public class BalanceBar : MonoBehaviour
 {
 	public RectTransform fillRect;
-	public float minValue = 0;
-	public float maxValue = 0;
+	[Range(-1, 1)]
 	public float value = 0;
 
+	Vector2 anchorMaxLeft = new Vector2(0.5f, 1);
+	Vector2 anchorMinLeft = new Vector2(0, 0);
+	Vector2 anchorMaxRight = new Vector2(1, 1);
+	Vector2 anchorMinRight = new Vector2(0.5f, 0);
 
+	private void Start()
+	{
+		fillRect.offsetMax = new Vector2(0, 0);
+		fillRect.offsetMin = new Vector2(0, 0);
+	}
+
+	private void Update()
+	{
+		if (value > 0)
+		{
+			fillRect.anchorMax = anchorMaxRight;
+			fillRect.anchorMin = anchorMinRight;
+		}
+		else
+		{
+			fillRect.anchorMax = anchorMaxLeft;
+			fillRect.anchorMin = anchorMinLeft;
+		}
+	}
 }
