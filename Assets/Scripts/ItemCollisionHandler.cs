c042f8064e8be70e36e4fa1b2c6cd061ece8a655
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2022-07-26 20:31:54 -0700

    made Balance Display scale with display resolution

--- Assets/Scripts/ItemCollisionHandler.cs
+++ Assets/Scripts/ItemCollisionHandler.cs
@@ -9,15 +9,12 @@ public class ItemCollisionHandler : MonoBehaviour
 	[SerializeField]
 	BucketControl leftBucket;
 	[SerializeField]
-	Slider leftBalance;
-	[SerializeField]
 	BucketControl rightBucket;
 	[SerializeField]
-	Slider rightBalance;
-
+	BalanceBar balanceDisplay;
 
 	private int leftBucketWeight = 0;
-	private int rightbucketWeight = 0;
+	private int rightBucketWeight = 0;
 	private int bucketBalance;
 	[SerializeField]
 	private int balanceCapacity = 10;
@@ -26,9 +23,6 @@ public class ItemCollisionHandler : MonoBehaviour
 	{
 		leftBucket.OnObjectCollected += OnObjectCollected;
 		rightBucket.OnObjectCollected += OnObjectCollected;
-
-		leftBalance.maxValue = balanceCapacity;
-		rightBalance.maxValue = balanceCapacity;
 	}
 
 	private void OnObjectCollected(string bucketName, GameObject collectedObject)
@@ -43,55 +37,35 @@ public class ItemCollisionHandler : MonoBehaviour
 			}
 			else
 			{
-				rightbucketWeight += collectedObject.GetComponent<ItemData>().itemValues.itemWeight;
+				rightBucketWeight += collectedObject.GetComponent<ItemData>().itemValues.itemWeight;
 			}
 		}
 
-		int balanceDirection = leftBucketWeight - rightbucketWeight;
+		float balanceDirection = leftBucketWeight - rightBucketWeight;
 
-		switch (balanceDirection)
-		{
-			case int expression when balanceDirection > 0:
-				leftBalance.value = balanceDirection;
-				rightBalance.value = 0;
-				break;
-			case int expression when balanceDirection < 0:
-				rightBalance.value = balanceDirection * -1;
-				leftBalance.value = 0;
-				break;
-			default:
-				Debug.Log("buckets are balanced" + balanceDirection);
-				leftBalance.value = 0;
-				rightBalance.value = 0;
-				break;
-		}
+		balanceDisplay.value = Mathf.Clamp((float)balanceDirection / (float)balanceCapacity, -1, 1);
 
 		if (balanceDirection < 0)
 		{
 			balanceDirection = balanceDirection * -1;
 		}
 
-		switch ((float)balanceDirection / (float)balanceCapacity)
+		switch (Mathf.Clamp((float)balanceDirection / (float)balanceCapacity, -1, 1))
 		{
 			case < 0.25f:
-				leftBalance.fillRect.GetComponent<Image>().color = Color.green;
-				rightBalance.fillRect.GetComponent<Image>().color = Color.green;
+				balanceDisplay.fillRect.GetComponent<Image>().color = Color.green;
 				break;
 			case < 0.5f:
-				leftBalance.fillRect.GetComponent<Image>().color = new Color(0.9803922f, 0.7176471f, 0.2f);
-				rightBalance.fillRect.GetComponent<Image>().color = new Color(0.9803922f, 0.7176471f, 0.2f);
+				balanceDisplay.fillRect.GetComponent<Image>().color = new Color(0.9803922f, 0.7176471f, 0.2f);
 				break;
 			case < 0.75f:
-				leftBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.5568628f, 0.08235294f);
-				rightBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.5568628f, 0.08235294f);
+				balanceDisplay.fillRect.GetComponent<Image>().color = new Color(1, 0.5568628f, 0.08235294f);
 				break;
 			case < 0.95f:
-				leftBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.3058824f, 0.06666667f);
-				rightBalance.fillRect.GetComponent<Image>().color = new Color(1, 0.3058824f, 0.06666667f);
+				balanceDisplay.fillRect.GetComponent<Image>().color = new Color(1, 0.3058824f, 0.06666667f);
 				break;
 			case > 0.95f:
-				leftBalance.fillRect.GetComponent<Image>().color = Color.red;
-				rightBalance.fillRect.GetComponent<Image>().color = Color.red;
+				balanceDisplay.fillRect.GetComponent<Image>().color = Color.red;
 				break;
 		}
 
