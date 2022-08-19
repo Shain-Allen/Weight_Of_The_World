15ca5117e083f9b71828645cbd92d8df45a06cd2
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2022-08-18 21:25:30 -0700

    made highscore timer

--- Assets/Scripts/OverlayController.cs
+++ Assets/Scripts/OverlayController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OverlayController : MonoBehaviour
 {
@@ -10,18 +11,50 @@ public class OverlayController : MonoBehaviour
 	[SerializeField]
 	private GameOverMenu gameOverDisplay;
 
+	[SerializeField]
+	private TextMeshProUGUI currentTimeText;
+	private float currentTime = 0;
+
+	[SerializeField]
+	private TextMeshProUGUI bestTimeText;
+	private float bestTime = 0;
+
+	private bool keepTime = false;
+
 
 	private void Start()
 	{
 		itemCH.OnGameOver += OnGameOver;
 
 		gameOverDisplay.gameObject.SetActive(false);
+
+		keepTime = true;
+
+		Debug.Log(PlayerPrefs.HasKey("BestTime"));
+
+		bestTime = PlayerPrefs.GetFloat("BestTime", 0);
+		bestTimeText.text = "Best Time: " + bestTime.ToString("#.00");
 	}
 
 	public void OnGameOver(string message)
 	{
-		//Debug.Log("yes, game over");
+		PlayerPrefs.SetFloat("BestTime", bestTime);
+		keepTime = false;
 		gameOverDisplay.gameObject.SetActive(true);
 		gameOverDisplay.gameOverDescription.text = message;
+	}
+
+	private void Update()
+	{
+		if (keepTime)
+		{
+			currentTime += Time.deltaTime;
+			if (currentTime > bestTime)
+			{
+				bestTime = currentTime;
+				bestTimeText.text = "Best Time: " + bestTime.ToString("#.00");
+			}
+			currentTimeText.text = "Current Time: " + currentTime.ToString("#.00");
+		}
 	}
 }
